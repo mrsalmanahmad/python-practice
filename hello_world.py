f9d4cfd37389fc2351a93d3c90d0b869cd023008
salmanahmad <salmanahmad3333@gmail.com>
2019-12-30 17:19:27 +0500

    py practice lesson 1

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+def hello():
+    print('Hello, World!')
+
+hello()
+print("hello \" salman ahmad \"")
+print("""helo lerm ipsim sdl 
+oolasm 
+sasollqw""")
+
+print (1+2)
+
+a = input("Your name")
+
+# concatination
+print (a + " hello how are you")
+print('spam'*3)
+print('2' + '2')
+print(int('2')+int('2'))
